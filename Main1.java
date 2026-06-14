8dfc0577e800a7047c2854e10646a7e730140e09
profedu_fbarreto <seu_email@github.com>
2026-06-14 18:37:21 -0300

    while

--- Main1.java
+++ Main1.java
@@ -4,12 +4,16 @@ public class Main1 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
         System.out.println("Você quer fazer a tabuada de qual número: ");
         int tabuada = teclado.nextInt();
 
-        for (int i = 0; i <= 10; i++) {
-            int resultado = tabuada * i;
-            System.out.println("O resultado de " + tabuada + " x " + i + " = " + resultado);
+        int i = 0;
+        while (i <= 10) {
+            int resultado = tabuada * numbers[i];
+            System.out.println("O resultado de: " + tabuada + " x " + numbers[i] + " = " + resultado);
+            i++;
         }
 
         teclado.close();
