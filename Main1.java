6373b68681c265332551359ba3d26974c9bf1e0b
profedu_fbarreto <seu_email@github.com>
2026-06-14 19:16:03 -0300

    funções

--- Main1.java
+++ Main1.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
 public class Main1 {
+
+    public static void saudacao(String nome) {
+        System.out.println("Olá " + nome);
+    }
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("Digite seu nome: ");
+        String nome = teclado.nextLine();
 
-        System.out.println("Você quer fazer a tabuada de qual número: ");
-        int tabuada = teclado.nextInt();
-
-        int i = 0;
-        while (i <= 10) {
-            int resultado = tabuada * numbers[i];
-            System.out.println("O resultado de: " + tabuada + " x " + numbers[i] + " = " + resultado);
-            i++;
-        }
+        saudacao(nome);
 
         teclado.close();
     }
