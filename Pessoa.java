ac01f743c56e3f646a5fc8207ecb3faf287fe48d
profedufbarreto <01289211027@senacrs.edu.br>
2026-08-04 19:20:09 -0300

    pessoa

---
+++ Pessoa.java
@@ -0,0 +1,31 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        if (idade > 0) {
+            this.idade = idade;
+        }
+    }
+
+    public void exibir() {
+        System.out.println("Nome: " + nome + " | Idade: " + idade);
+    }
+}
